a48c7d4f1eb94d6a05de75fc519b732686bb988b
Korsar21 <114531160+Korsar21@users.noreply.github.com>
2025-03-22 12:19:34 +0300

    try to start on ower device

---
+++ FAK/build/CMakeFiles/FAK.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FAK_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/FAK_autogen.dir/ParseCache.txt"
+  "FAK_autogen"
+  "CMakeFiles/FAK.dir/FAK_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/FAK.dir/FAK_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/FAK.dir/main.cpp.o"
+  "CMakeFiles/FAK.dir/main.cpp.o.d"
+  "CMakeFiles/FAK.dir/mainwindow.cpp.o"
+  "CMakeFiles/FAK.dir/mainwindow.cpp.o.d"
+  "FAK"
+  "FAK.pdb"
+  "FAK_autogen/mocs_compilation.cpp"
+  "FAK_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FAK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
