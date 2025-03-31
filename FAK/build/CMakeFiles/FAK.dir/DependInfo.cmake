ef5ab92d7419e0e206555067484b22f332fdffc8
Korsar21 <114531160+Korsar21@users.noreply.github.com>
2025-03-31 18:51:51 +0300

    try decide problem 2

---
+++ FAK/build/CMakeFiles/FAK.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "FAK_autogen/timestamp" "custom" "FAK_autogen/deps"
+  "/home/korsar/chest/GitHub/FAK/FAK/build/FAK_autogen/EWIEGA46WW/qrc_resources.cpp" "CMakeFiles/FAK.dir/FAK_autogen/EWIEGA46WW/qrc_resources.cpp.o" "gcc" "CMakeFiles/FAK.dir/FAK_autogen/EWIEGA46WW/qrc_resources.cpp.o.d"
+  "/home/korsar/chest/GitHub/FAK/FAK/build/FAK_autogen/mocs_compilation.cpp" "CMakeFiles/FAK.dir/FAK_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/FAK.dir/FAK_autogen/mocs_compilation.cpp.o.d"
+  "/home/korsar/chest/GitHub/FAK/FAK/classes/myDayTasks.cpp" "CMakeFiles/FAK.dir/classes/myDayTasks.cpp.o" "gcc" "CMakeFiles/FAK.dir/classes/myDayTasks.cpp.o.d"
+  "/home/korsar/chest/GitHub/FAK/FAK/main.cpp" "CMakeFiles/FAK.dir/main.cpp.o" "gcc" "CMakeFiles/FAK.dir/main.cpp.o.d"
+  "/home/korsar/chest/GitHub/FAK/FAK/mainwindow.cpp" "CMakeFiles/FAK.dir/mainwindow.cpp.o" "gcc" "CMakeFiles/FAK.dir/mainwindow.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
