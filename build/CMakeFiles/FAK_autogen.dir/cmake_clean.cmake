85166d3426ba971c4fe03681ab52596e3394eec8
Teddy-Ro <rozanovfedya@gmail.com>
2025-05-05 00:19:47 +0300

    изменен стиль

---
+++ build/CMakeFiles/FAK_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FAK_autogen"
+  "FAK_autogen/include/classes/ui_myDayTasks.h"
+  "FAK_autogen/include/ui/ui_importantList.h"
+  "FAK_autogen/include/ui/ui_planned.h"
+  "FAK_autogen/include/ui_mainwindow.h"
+  "FAK_autogen/mocs_compilation.cpp"
+  "FAK_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/FAK_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
